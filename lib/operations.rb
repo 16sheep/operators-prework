f681749305883482b07fead716b7a24609dc0b23
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-05 16:28:21 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-  if speed > 60 && speed < 40
+  if speed > 60 || speed < 40
     TRUE
   else
     FALSE
@@ -9,7 +9,7 @@ end
 
 
 def not_safe?(speed)
-	speed > 60 && speed < 40 ? true : false
+	speed > 60 || speed < 40 ? true : false
 end
 	
 
